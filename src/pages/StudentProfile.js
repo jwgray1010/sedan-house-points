cf59217adc9a09792121d6351799b22232876063
jwgray1010 <john.gray@usd286.org>
2025-06-05 05:13:52 +0000

    Add StudentProfile.js page

---
+++ src/pages/StudentProfile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const StudentProfile = () => (
+  <div style={{ textAlign: 'center', marginTop: '3rem' }}>
+    <h1>Student Profile</h1>
+    <p>This page will show the student's profile details.</p>
+  </div>
+);
+
+export default StudentProfile;
