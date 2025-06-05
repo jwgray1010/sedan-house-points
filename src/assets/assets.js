2cb224517ea6dbb83aec9f768f85748fa35a385c
jwgray1010 <john.gray@usd286.org>
2025-06-05 13:06:03 +0000

    Fix asset imports and exports for Netlify deployment

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,11 +3,13 @@ import houseStorm from './house_storm.png';
 import houseMeadow from './house_meadow.png';
 import houseFlint from './house_flint.png';
 import houseEmber from './house_ember.png';
+import sedanLogo from './sedan_logo.png';
 
 export {
   glowingShieldLogo,
   houseStorm,
   houseMeadow,
   houseFlint,
-  houseEmber
+  houseEmber,
+  sedanLogo
 };
