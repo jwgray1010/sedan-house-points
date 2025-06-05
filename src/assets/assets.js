bbbe2be1ab6ce2097cc25df13a8f2895af43e971
jwgray1010 <john.gray@usd286.org>
2025-06-05 07:01:46 +0000

    fix: use ES module imports for assets

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,6 +1,15 @@
-export const glowingShieldLogo = require('./glowing_logo.png');
-export const houseStorm = require('./house_storm.png');
-export const houseMeadow = require('./house_meadow.png');
-export const houseFlint = require('./house_flint.png');
-export const houseEmber = require('./house_ember.png');
-export const sedanLogo = require('./sedan_logo.png');
+import glowingShieldLogo from './glowing_logo.png';
+import houseStorm from './house_storm.png';
+import houseMeadow from './house_meadow.png';
+import houseFlint from './house_flint.png';
+import houseEmber from './house_ember.png';
+import sedanLogo from '../sedan_logo.js';
+
+export {
+  glowingShieldLogo,
+  houseStorm,
+  houseMeadow,
+  houseFlint,
+  houseEmber,
+  sedanLogo
+};
